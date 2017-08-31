ea3b5653be6a22ad52a4e08c9cd20be5241dc54b
Eugenio <eyakhnenko@8amarketing.com>
2017-08-31 18:17:10 -0300

    bugfix in query engine and adding docs

--- js/query.js
+++ js/query.js
@@ -1,5 +1,11 @@
+/**
+ * Basic javascript dom traversing utility
+ * @param  {string|object} selector A selector for querySelect or dom object
+ * @param  {boolean} limitOne Optional, set to true if you want to query a single element
+ * @return {object}          Objects with various methods to modify the dom
+ */
 function query(selector, limitOne){
-	var limitOne = (typeof limitOne === "boolean") ? limitOne : false;
+
 	var emptyNodeList = function(nodeList){
 		return nodeList.length == 0;
 	};
@@ -8,6 +14,10 @@ function query(selector, limitOne){
 			return false;
 		}
 		for(var i = 0; i < args.length; ++i){
+			if(arguments[i+1] === "string"){
+				// cast to string
+				args[i] = (args[i]).toString();
+			}
 			if(typeof args[i] !== arguments[i+1]) return false;
 		}
 		return true;
@@ -15,42 +25,38 @@ function query(selector, limitOne){
 	var emptyArguments = function(args){
 		return args.length == 0;
 	};
-	
 
+	var limitOne = (typeof limitOne === "boolean") ? limitOne : false;
 	var elements =  [];
-	if (typeof selector !== "string"){
-		console.error(selector + " is not a string, 'query' requires a string as selector");
-		selector = null;
-	} else {
+
+	if (typeof selector === "string"){
+		// selector is  a query string
 		if(limitOne){
 			elements.push(document.querySelector(selector));
 		} else {
 			elements = document.querySelectorAll(selector)
 		}
+	} else if(typeof selector === "object"){
+		//selector is an dom object "hopefully"
+		elements.push(selector);
+		selector = false;
+	} else {
+		// selector is not a string nor a dom object
+		console.error(selector + " is not a string, 'query' requires a string as selector");
+		selector = null;
 	}
 
 	return {
 		selector: selector,
 		nodes: elements,
 		length: elements.length,
-		query: function(selector, limitOne){
-			var limitOne = (typeof limitOne === "boolean") ? limitOne : false;
-			if (typeof selector !== "string"){
-				console.error(selector + " is not a string, 'query' requires a string as selector");
-				selector = null;
-				this.nodes = [];
-				return this;
-			}
-			this.selector = selector;
-			if(limitOne){
-				this.nodes = [];
-				this.nodes.push(document.querySelector(selector));
-			} else {
-				this.nodes = document.querySelectorAll(selector)
-			}
-			this.length = this.nodes.length;
-			return this;
-		},
+
+		/**
+		 * Adds event listener
+		 * @param  {string} event  Type of the event to listen to
+		 * @param  {function} method Method to execute on the event
+		 * @return {object}        Query object for nesting
+		 */
 		on: function(event, method){
 			if(emptyNodeList(this.nodes)) {
 				console.error("No elements with selector: " + this.selector + ' for on method');
@@ -66,15 +72,18 @@ function query(selector, limitOne){
 			}
 			return this;
 		},
+		/**
+		 * Iterates over the list of dom nodes
+		 * @param  {function} method A function to execute for each node, 
+		 *   "this" is gonna be set to the current iterated element
+		 * @return {object}        Query object for nesting
+		 */
 		each: function(method){
 			if(emptyNodeList(this.nodes)) {
 				console.error("No elements with selector: " + this.selector + ' for each');
 				return this;
 			}
-			if(
-				!validArguments(arguments, "function") || 
-				!emptyArguments(arguments)
-			){
+			if(!validArguments(arguments, "function")){
 				console.error(method + " is not a function, 'each' requires a function as argument");
 				return this;
 			}
@@ -83,7 +92,13 @@ function query(selector, limitOne){
 			}
 			return this;
 		},
-		html: function(string){
+		/**
+		 * Sets the innerHTML of each elements in the list or
+		 * Gets the value of innerHTML of the first element if no arguments
+		 * @param  {string} value Optional, the new innerHTML value
+		 * @return {mixed}        Query object for nesting or value if getter
+		 */
+		html: function(value){
 			if(emptyNodeList(this.nodes)) {
 				console.error("No elements with selector: " + this.selector + ' for html');
 				return this;
@@ -92,15 +107,21 @@ function query(selector, limitOne){
 				return this.nodes[0].innerHTML;
 			}
 			if(!validArguments(arguments, "string")){
-				console.error("'html' takes string{string} as argument or no arguments.");
+				console.error("'html' takes value{string} as argument or no arguments.");
 				return this;
 			}
 			for (var i = 0; i < this.nodes.length; i++) {
-				this.nodes[i].innerHTML = string;
+				this.nodes[i].innerHTML = value;
 			}
 			return this;
 		},
-		text: function(string){
+		/**
+		 * Sets the textContent of each elements in the list or
+		 * Gets the value of textContent of the first element if no arguments
+		 * @param  {string} value Optional, the new textContent value
+		 * @return {mixed}        Query object for nesting or value if getter
+		 */
+		text: function(value){
 			if(emptyNodeList(this.nodes)) {
 				console.error("No elements with selector: " + this.selector + ' for text');
 				return this;
@@ -109,15 +130,54 @@ function query(selector, limitOne){
 				return this.nodes[0].textContent;
 			}
 			if(!validArguments(arguments, "string")){
-				console.error("'text' takes string{string} as argument or no arguments.");
+				console.error("'text' takes value{string} as argument or no arguments.");
 				return this;
 			}
 			for (var i = 0; i < this.nodes.length; i++) {
-				this.nodes[i].textContent = string;
+				this.nodes[i].textContent = value;
 			}
 			return this;
 		},
-		value: function(string){
+		/**
+		 * Sets the attribute of each elements in the list or
+		 * Gets the value of attribute of the first element if no arguments
+		 * @param {string} attr Attribute to be set
+		 * @param  {string} value Optional, the new attribute value
+		 * @return {mixed}        Query object for nesting or value if getter
+		 */
+		attr: function(attr, value){
+			if(emptyNodeList(this.nodes)) {
+				console.error("No elements with selector: " + this.selector + ' for text');
+				return this;
+			}
+			if(emptyArguments(arguments)){
+				console.error("'attr' requires at least one argument as attribute{string}");
+				return this;
+			}
+			if(arguments.length == 1){
+				if(!validArguments(arguments, "string")){
+					console.error("'attr' takes attribute{string} as argument for getter");
+					return this;
+				}	
+				return this.nodes[0].getAttribute(attr);
+			}
+
+			if(!validArguments(arguments, "string", "string")){
+				console.error("'attr' takes two attribute{string}, value{string} as setter");
+				return this;
+			}	
+			for (var i = 0; i < this.nodes.length; i++) {
+				this.nodes[i].setAttribute(attr, value);
+			}
+			return this;
+		},
+		/**
+		 * Sets the value of each elements in the list or
+		 * Gets the value of value of the first element if no arguments
+		 * @param  {string} val Optional, the new value value
+		 * @return {object}        Query object for nesting
+		 */
+		value: function(val){
 			if(emptyNodeList(this.nodes)) {
 				console.error("No inputs with selector: " + this.selector + ' for value');
 				return this;
@@ -126,45 +186,49 @@ function query(selector, limitOne){
 				return this.nodes[0].value;
 			}
 			if(!validArguments(arguments, "string")){
-				console.error("'value' takes string{string} as argument or no arguments.");
+				console.error("'value' takes value{string} as argument or no arguments.");
 				return this;
 			}
 			for (var i = 0; i < this.nodes.length; i++) {
-				this.nodes[i].value = string;
+				this.nodes[i].value = val;
 			}
 			return this;
 		},
-		append: function(string){
+		/**
+		 * Appends a string to each element in the list
+		 * @param  {string} value String to be apended
+		 * @return {object}        Query object for nesting
+		 */
+		append: function(value){
 			if(emptyNodeList(this.nodes)) {
 				console.error("No elements with selector: " + this.selector + ' for append');
 				return this;
 			}
-			if(
-				!validArguments(arguments, "string") && 
-				!emptyArguments(arguments)
-			){
+			if(!validArguments(arguments, "string")){
 				console.error("'append' takes string{string} as argument");
 				return this;
 			}
 			for (var i = 0; i < this.nodes.length; i++) {
-				this.nodes[i].innerHTML += string;
+				this.nodes[i].innerHTML += value;
 			}
 			return this;
 		},
-		prepend: function(string){
+		/**
+		 * Prepends a string to each element in the list
+		 * @param  {string} value String to be apended
+		 * @return {object}        Query object for nesting
+		 */
+		prepend: function(value){
 			if(emptyNodeList(this.nodes)) {
 				console.error("No elements with selector: " + this.selector + ' for prepend');
 				return this;
 			}
-			if(
-				!validArguments(arguments, "string") && 
-				!emptyArguments(arguments)
-			){
+			if(!validArguments(arguments, "string")){
 				console.error("'prepend' takes string{string} as argument");
 				return this;
 			}
 			for (var i = 0; i < this.nodes.length; i++) {
-				this.nodes[i].innerHTML = string + this.nodes[i].innerHTML;
+				this.nodes[i].innerHTML = value + this.nodes[i].innerHTML;
 			}
 			return this;
 		}
